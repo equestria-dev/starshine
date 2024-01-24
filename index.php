617263aeadfc549f7c0598c2c5f5bb56b4b7083e
RaindropsSys <raindrops@equestria.dev>
2024-01-24 18:52:59 +0100

    Update translations and bug fixes

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
             </div>
         </div>
         <div id="hero-logo" class="container">
-            <video id="hero-logo-img" src="/assets/img/intro.webm" autoplay></video>
+            <video id="hero-logo-img" src="/assets/img/intro.webm" muted autoplay></video>
         </div>
     </div>
 </div>
