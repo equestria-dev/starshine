88e17668d301935069b6d5e242f5bdf566bbf5f6
RaindropsSys <raindrops@equestria.dev>
2024-02-04 13:41:55 +0100

    Updated 4 files (automated)

--- assets/src/main.js
+++ assets/src/main.js
@@ -31,7 +31,7 @@ window.onload = () => {
     }
 
     if (document.getElementById("hero-logo-img")) document.getElementById("hero-logo-img").ontimeupdate = () => {
-        document.getElementById("hero-logo-img").disableRemotePlayback();
+        try { document.getElementById("hero-logo-img").disableRemotePlayback(); } catch (e) {}
         if (document.getElementById("hero-logo-img").duration - document.getElementById("hero-logo-img").currentTime <= .7) {
             document.body.classList.add("animated");
         }
